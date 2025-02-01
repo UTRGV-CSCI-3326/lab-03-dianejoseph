2dca02bac756239601909500c9c8c42b308cd983
Diane Joseph <104699671+dianejoseph@users.noreply.github.com>
2025-02-01 21:52:22 +0000

    first commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,9 @@
-
+public class BankAccount{
+    public static void main(String[] args){
+        double balance = 2175.37;
+        balance = balance - 302.5 + 50.03;
+        balance = balance/2 + 20 - 1;
+        balance = balance * 2 + 1;
+        System.out.print(balance);
+    }
+}
